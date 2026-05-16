fe4abe0b3e0cdba77dde440c58a8c96c9adc1d8a
dskst <10830352+dskst@users.noreply.github.com>
2026-05-17 01:00:19 +0900

    feat: add neovim of copilot

---
+++ .config/nvim/lua/plugins/ai.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-y>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+      panel = {
+        enabled = true,
+      },
+    },
+  },
+}
